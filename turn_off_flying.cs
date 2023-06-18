15b5d2893174c54b0302d9ee9b768fc6ac0525bc
Shawn Nassabi <128087989+shawn-nassabi@users.noreply.github.com>
2023-06-17 23:53:15 -0700

    Update turn_off_flying.cs

--- turn_off_flying.cs
+++ turn_off_flying.cs
@@ -12,11 +12,7 @@ public class turn_off_flying : MonoBehaviour
         thisAudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
